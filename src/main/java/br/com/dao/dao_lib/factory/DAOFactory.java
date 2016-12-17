dbe89da9f44742c552e1ee2655c2b586219a5407
niccolas <niccolas.costa@gmail.com>
2016-12-17 17:47:14 -0200

    commit inicial

---
+++ src/main/java/br/com/dao/dao_lib/factory/DAOFactory.java
@@ -0,0 +1,26 @@
+package br.com.dao.dao_lib.factory;
+
+import java.lang.reflect.ParameterizedType;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import br.com.dao.dao_lib.dao.DAO;
+
+@SuppressWarnings("unchecked")
+public class DAOFactory {
+	
+	@Inject
+	private EntityManager manager;
+
+	@Produces
+	public <T> DAO<T> factory(InjectionPoint point){
+		ParameterizedType type = (ParameterizedType)point.getType();
+		
+		Class<T> classe = (Class<T>) type.getActualTypeArguments()[0];
+		return new DAO<>(classe, manager);
+	}
+
+}
